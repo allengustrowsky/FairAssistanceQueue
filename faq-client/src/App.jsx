e3af3b529f4ea4703f6b031c119ae858c7f51486
Allen Gustrowsky <allen@may21.us>
2022-12-06 21:55:55 -0500

    Basic connection between front and back end

--- faq-client/src/App.jsx
+++ faq-client/src/App.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [testData, setTestData] = useState('')
+  useEffect(() => {
+    const getData = async () => {
+        const raw = await fetch('http://127.0.0.1:5000/members')
+        console.log(raw)
+        const jsonData = await raw.json()
+        console.log(jsonData)
+        setTestData(jsonData)
+
+    }
+    getData()
+  }, [])
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Test call page</h1>
+        {testData === '' ? 
+        <h3>Loading...</h3>
+        : 
+        testData['members'].map((member, index) => {
+            return <p key={index}>{member}</p>
+        })
+
+        }
     </div>
   )
 }
